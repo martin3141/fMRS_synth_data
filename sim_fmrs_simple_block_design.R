7c6102f3d37d43631e7b82f0499a959c807b6593
Martin Wilson <martin@pipegrep.co.uk>
2024-05-28 12:31:08 +0100

    code refactor

--- sim_fmrs_simple_block_design.R
+++ sim_fmrs_simple_block_design.R
@@ -6,81 +6,27 @@ library(spant)
 seq_tr      <- 2    # set the sequence TR
 N_scans     <- 448  # just under 15 mins with TR = 2s, but still divisible by 64
 bz_inhom_lb <- 4    # Gaussian line-broadening to simulate B0 inhomogeneity
-bold_lb_hz  <- 0.2  # linewidth differences in Hz from BOLD T2* effect
+bold_lb_hz  <- 0.0  # linewidth differences in Hz from BOLD T2* effect
 noise_level <- 10   # frequency domain noise standard deviation added to taste
 set.seed(1)         # random number generator seed
 
 # Make a data frame containing a single row of basis signal amplitudes.
 # Metabolite values are for visual cortex listed in Bednarik et al 2015 Table 1.
 # Note Alanine and Glycine are not listed in the table and therefore set to 0.
-basis_amps <- data.frame("ala" = 0, "asc" = 0.96, "asp" = 3.58, "cr" = 4.22,
+basis_amps <- data.frame("ala" = 0.00, "asc" = 0.96, "asp" = 3.58, "cr" = 4.22,
                          "gaba" = 1.03, "glc" = 0.62, "gln" = 2.79,
                          "gly" = 0.00, "gsh" = 1.09, "glu" = 8.59, "gpc" = 0.54,
-                         "ins" = 6.08, "lac" = 1.01, "naa" = 11.9,
+                         "ins" = 6.08, "lac" = 1.01, "naa" = 11.90,
                          "naag" = 1.32, "pch" = 0.40, "pcr" = 3.34,
                          "peth" = 0.93, "sins" = 0.27, "tau" = 1.27,
-                         "lip09" = 0, "lip13a" = 0, "lip13b" = 0, "lip20" = 0,
-                         "mm09" = 4, "mm12" = 4, "mm14" = 4, "mm17"= 4,
-                         "mm20" = 4)
+                         "lip09" = 0.00, "lip13a" = 0.00, "lip13b" = 0.00,
+                         "lip20" = 0.00, "mm09" = 4.00, "mm12" = 4.00,
+                         "mm14" = 4.00, "mm17"= 4.00, "mm20" = 4.00)
 
 # Duplicate the row N_scans times
 basis_amps <- basis_amps[rep(1, N_scans),]
 
-# Simulate a typical basis for TE=28ms semi-LASER acquisition at 3T
-acq_paras <- def_acq_paras(ft = 127.8e6)
-basis     <- sim_basis(names(basis_amps), pul_seq = seq_slaser_ideal,
-                       TE1 = 0.008, TE2 = 0.011, TE3 = 0.009)
-
-mrs_dyn_orig <- basis2dyn_mrs_data(basis, basis_amps, seq_tr)
-
-
-break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# XXXX
-mrs_data <- basis2mrs_data(basis, amps = basis_amps, sum_elements = TRUE)
-
-
-# Simulate a typical basis for TE=28ms semi-LASER acquisition at 3T
-acq_paras <- def_acq_paras() # field strength could be adjusted here
-brain_sim <- sim_brain_1h(acq_paras, full_output = TRUE, TE1 = 0.008,
-                          TE2 = 0.011, TE3 = 0.009, pul_seq = seq_slaser_ideal)
-
-# Use more accurate baseline concentration estimates for visual cortex from 
-# Bednarik et al 2015 Table 1. Note Ascorbate is not simulated here, but likely
-# small enough to ignore at 3T.
-amps  <- brain_sim$amps
-amps["Ala"]  <- 0.00 # not listed in Bednarik et al, so set to zero
-amps["Asp"]  <- 3.58
-amps["Cr"]   <- 4.22
-amps["GABA"] <- 1.03
-amps["Glc"]  <- 0.62
-amps["Gln"]  <- 2.79
-amps["Gly"]  <- 0.00 # not listed in Bednarik et al, so set to zero
-amps["GSH"]  <- 1.09
-amps["Glu"]  <- 8.59
-amps["GPC"]  <- 0.54
-amps["Ins"]  <- 6.08
-amps["Lac"]  <- 1.01
-amps["NAA"]  <- 6.08
-amps["NAAG"] <- 1.32
-amps["PCh"]  <- 0.40
-amps["PCr"]  <- 3.34
-amps["PEth"] <- 0.93
-amps["sIns"] <- 0.27
-amps["Tau"]  <- 1.27
+# Update the amplitudes according to predicted dynamics
 
 # From Bednarik et at 2015 Table 1.
 lac_perc_change <-  29.6
@@ -88,57 +34,48 @@ glu_perc_change <-  3.3
 glc_perc_change <- -16.0
 asp_perc_change <- -5.4
 
-# generate a data frame of baseline metabolite level for each dynamic scan
-amps_df <- data.frame(t(amps))
-amps_df <- amps_df[rep(1, N_scans),]
-
-# generate a dummy mrs_data object to aid metabolite response fn generation
-mrs_data_dummy <- brain_sim$mrs_data |> set_tr(seq_tr) |> set_Ntrans(N_scans) |>
-                  rep_dyn(N_scans)
-
-# generate metabolite response functions assuming simple trapezoidal shapes with
-# 30 second rise time and 120 second fall time
 # simulate two 120 second blocks of stimulation starting at 100 and 500 seconds
 onsets    <- c(100, 500)
 durations <- rep(120, 2)
-labels    <- rep("x", 2)
 
-glu_rf <- gen_trap_reg(onsets, durations, labels, mrs_data_dummy,
+# generate a dummy mrs_data object for generating the regressors
+mrs_data_dummy <- sim_resonances() |> set_tr(seq_tr) |> set_Ntrans(N_scans) |>
+                  rep_dyn(N_scans)
+
+# generate metabolite response functions assuming simple trapezoidal shapes
+glu_rf <- gen_trap_reg(onsets, durations, mrs_data = mrs_data_dummy,
                        rise_t = 120, fall_t = 150)
 
-lac_rf <- gen_trap_reg(onsets, durations, labels, mrs_data_dummy,
+lac_rf <- gen_trap_reg(onsets, durations, mrs_data = mrs_data_dummy,
                        rise_t = 120, fall_t = 150)
 
-asp_rf <- gen_trap_reg(onsets, durations, labels, mrs_data_dummy,
+asp_rf <- gen_trap_reg(onsets, durations, mrs_data = mrs_data_dummy,
                        rise_t = 120, fall_t = 150)
 
-glc_rf <- gen_trap_reg(onsets, durations, labels, mrs_data_dummy,
+glc_rf <- gen_trap_reg(onsets, durations, mrs_data = mrs_data_dummy,
                        rise_t = 120, fall_t = 150)
 
-bold_rf <- gen_bold_reg(onsets, durations, labels, mrs_data_dummy)
+bold_rf <- gen_bold_reg(onsets, durations, mrs_data = mrs_data_dummy)
 
 # update metabolite data frame to have dynamic metabolite values
-amps_df$Glu <- amps_df$Glu * (glu_rf$x * glu_perc_change / 100 + 1)
-amps_df$Lac <- amps_df$Lac * (lac_rf$x * lac_perc_change / 100 + 1)
-amps_df$Asp <- amps_df$Asp * (asp_rf$x * asp_perc_change / 100 + 1)
-amps_df$Glc <- amps_df$Glc * (glc_rf$x * glc_perc_change / 100 + 1)
+basis_amps$glu <- basis_amps$glu * (glu_rf$stim * glu_perc_change / 100 + 1)
+basis_amps$lac <- basis_amps$lac * (lac_rf$stim * lac_perc_change / 100 + 1)
+basis_amps$asp <- basis_amps$asp * (asp_rf$stim * asp_perc_change / 100 + 1)
+basis_amps$glc <- basis_amps$glc * (glc_rf$stim * glc_perc_change / 100 + 1)
 
-# generate a list of spectra based on dynamic metabolite values
-mrs_list <- vector(mode = "list", length = N_scans)
-for (n in 1:N_scans) {
-  amps_n <- amps_df[n,] |> as.numeric()
-  mrs_list[[n]] <- basis2mrs_data(brain_sim$basis, sum_elements = TRUE,
-                                  amps = amps_n)
-}
+# aimulate a typical basis for TE=28ms semi-LASER acquisition at 3T
+acq_paras <- def_acq_paras(ft = 127.8e6)
+basis     <- sim_basis(names(basis_amps), pul_seq = seq_slaser_ideal,
+                       TE1 = 0.008, TE2 = 0.011, TE3 = 0.009)
 
-# convert list of spectra to a single dynamic scan and set timing parameters
-mrs_dyn_orig <- append_dyns(mrs_list) |> set_tr(seq_tr) |> set_Ntrans(N_scans) 
+# apply basis amplitudes to the basis set to generate a simulated fMRS dataset
+mrs_dyn_orig <- basis2dyn_mrs_data(basis, basis_amps, seq_tr)
 
 # broaden basis to simulate B0 inhomogeneity, apply any addition BOLD related 
 # narrowing and add noise
-bold_lb_dyn <- (1 - bold_rf$x) * bold_lb_hz
-mrs_dyn <- mrs_dyn_orig |> lb(bz_inhom_lb) |> 
-  lb(bold_lb_dyn, 0) |> add_noise(noise_level)
+bold_lb_dyn <- (1 - bold_rf$stim_bold) * bold_lb_hz
+mrs_dyn <- mrs_dyn_orig |> lb(bz_inhom_lb) |>  lb(bold_lb_dyn, 0) |>
+  add_noise(noise_level)
 
 # plots
 mrs_dyn |> lb(4) |> sub_mean_dyns() |> image(xlim = c(4, 0.5))
@@ -150,14 +87,14 @@ mrs_dyn |> lb(4) |> mean_dyn_blocks(32) |> sub_mean_dyns() |>
 # generate a Boxcar function to describe the task
 # this isn't optimal, but maybe useful in the case where only minimal
 # assumptions can be made about the metabolite response function
-boxcar_rf <- gen_trap_reg(onsets, durations, labels, mrs_data_dummy, rise_t = 0,
-                          fall_t = 0)
+boxcar_rf <- gen_trap_reg(onsets, durations, mrs_data = mrs_data_dummy,
+                          rise_t = 0, fall_t = 0)
 
-plot(boxcar_rf$time, boxcar_rf$x, type = "l")
-lines(boxcar_rf$time, glu_rf$x, col = "red")
-lines(boxcar_rf$time, bold_rf$x, col = "blue")
+plot(boxcar_rf$time, boxcar_rf$stim, type = "l")
+lines(boxcar_rf$time, glu_rf$stim, col = "red")
+lines(boxcar_rf$time, bold_rf$stim_bold, col = "blue")
 
-task_bool <- boxcar_rf$x > .5
+task_bool <- boxcar_rf$stim > .5
 task_inds <- which(task_bool)
 rest_inds <- which(!task_bool)
 
